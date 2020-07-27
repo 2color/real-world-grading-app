e771143e22854e8351facded5bcee4428a353e02
Daniel Norman <daniel@2color.me>
2020-07-27 13:40:52 +0200

    Disconnect Prisma when server is stopped

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -19,6 +19,15 @@ const prismaPlugin: Hapi.Plugin<null> = {
     })
 
     server.app.prisma = prisma
+
+    // Close DB connection after the server's connection listeners are stopped
+    // Related issue: https://github.com/hapijs/hapi/issues/2839
+    server.ext({
+      type: 'onPostStop',
+      method: async (server: Hapi.Server) => {
+        server.app.prisma.disconnect()
+      },
+    })
   },
 }
 
